5e66fcc298f16eae1e5b6b3410aa142ce80a24d9
Ziad <84142720+Ziad-El3assal@users.noreply.github.com>
2024-07-06 15:17:43 +0300

    makin the automation  robust for Error in data so it does not need monitoring anymore

--- run.bat
+++ run.bat
@@ -1,5 +1,5 @@
 @echo off
-setlocal
+setlocal enabledelayedexpansion
 
 REM Check for the correct number of arguments
 if "%~2"=="" (
@@ -27,12 +27,11 @@ REM Run the server.py script in a new command prompt and close it after it is do
 
 @echo off
 setlocal
-#function that get PID and kill it
 
 
 
 REM Start the server and get its PID
-start "SERVER" cmd /k "python server.py %number_clients% & taskkill /f /im cmd.exe"
+start "SERVER" /MIN cmd /k "python server.py %number_clients% & taskkill /f /im cmd.exe"
 timeout /t 5 /nobreak
 
 for /f "tokens=2 " %%a in ('tasklist /fi "WindowTitle eq SERVER*"') do (
@@ -48,18 +47,30 @@ for /L %%i in (1,1,%number_clients%) do (
     set "file_path=Data\split_%%i.csv"
     echo python client.py %%i %file_path%
     call conda activate flowerTutorial
-    start "Client %%i" cmd /k "python client.py %%i Data\split_%%i.csv"
+    start "Client %%i" /MIN cmd /k "python client.py %%i Data\split_%%i.csv"
 )
 
+set MaxTime=10  
+set TimeElapsed=0
 
 :loop
-tasklist | find " %server_pid% " >nul
-if not errorlevel 1  (
-    timeout /t 10 >nul
-    @echo on
-    echo Server is still running
-    @echo off
-    goto :loop
+tasklist | find "%server_pid%" >nul
+if %errorlevel% neq 0 (
+    echo Server process with PID %server_pid% is not running.
+    taskkill /F /IM cmd.exe /T
+    goto end
+)
+if %TimeElapsed% geq %MaxTime% (
+    echo Reached maximum wait time. Server process with PID %server_pid% is still running.
+    taskkill /F /IM cmd.exe /T
+    goto end
 )
 
+echo Waiting for the server to stop %TimeElapsed%/%MaxTime%
+timeout /t 10 >nul
+set /a TimeElapsed+=1
+goto loop
+:end
+echo Finished checking server status.
+exit /b
 endlocal
